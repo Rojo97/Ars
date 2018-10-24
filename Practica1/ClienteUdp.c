3217bbb6991716d8aa86f5e251c7d9073adf1db8
Rojo97 <victorrojo1997@gmail.com>
2018-10-24 11:37:20 +0200

    Control de errores y eliminacion de warnings

--- Practica1/ClienteUdp.c
+++ Practica1/ClienteUdp.c
@@ -8,6 +8,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ifaddrs.h>
+#include <unistd.h>
+
+void error(char *funcion){
+    perror(funcion);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -15,52 +21,62 @@ int main(int argc, char const *argv[])
     struct in_addr ip;
     int descriptor = 0;
     char cadena[] = "Hola";
-    struct ifaddrs *localdir;
-    struct ifaddrs *tmpdir;
     struct sockaddr_in localIp;
     struct sockaddr_in addr;
-    char *mensaje;
-    if(argc < 2|| argc > 4 || argc == 3){
-        printf("Numero de argumentos incorrecto");//colocar bien
-        fflush(stdout);
-        return(-1);
+    char *mensaje = "";
+    if(argc == 2){
+        struct servent *portServent = getservbyname("qotd", "udp");
+        if( portServent == NULL){
+            error("getservbyname()");
+        }
+        port = portServent[0].s_port;
     }else if(argc == 4){
         if(strcmp(argv[2], "-p") != 0){
             printf("Uso incorrecto, para seleccionar puerto indiquelo con -p");//colocar bien o usar por defecto
             fflush(stdout);
             return(-1);
         } else {
-            sscanf(argv[3],"%d", &port);
+            sscanf(argv[3],"%hd", &port);
             port = htons(port);
             if(port <0){
-                perror("htons()");
-                exit(EXIT_FAILURE);
+                error("htons()");
             }
         }
     } else {
-        struct servent *portServent = getservbyname("qotd", "udp");
-        if( portServent == NULL){
-            perror("getservbyname()");
-            exit(EXIT_FAILURE);
-        }
-        port = portServent[0].s_port;
+        printf("Numero de argumentos incorrecto");//colocar bien
+        fflush(stdout);
+        return(-1);
     }
     int inetVar = inet_aton(argv[1], &ip);
     if(inetVar <0){
-        perror("inet_atom()");
-        exit(EXIT_FAILURE);
+        error("inet_atom()");
     }
-
-    descriptor = socket(AF_INET,SOCK_DGRAM,0); //Error
+    descriptor = socket(AF_INET,SOCK_DGRAM,0);
+    if(descriptor<0){
+        error("socket()");
+    }
     localIp.sin_family = AF_INET;
     localIp.sin_addr.s_addr = INADDR_ANY;
-    bind(descriptor, (struct sockaddr *) &localIp,sizeof(localIp));
+    int errbind = bind(descriptor, (struct sockaddr *) &localIp,sizeof(localIp));
+    if(errbind){
+        error("bind()");
+    }
     addr.sin_family = AF_INET;
     addr.sin_port = port;
-    addr.sin_addr.s_addr = &ip;
-    sendto(descriptor,&cadena,sizeof(char)*strlen(cadena),0, (struct sockaddr*)&addr, sizeof(addr));
-    recvfrom(descriptor, mensaje, sizeof(char)*512,0, (struct sockaddr*)&addr, sizeof(addr) );
+    addr.sin_addr.s_addr = ip.s_addr;
+    socklen_t sizeaddr = sizeof(addr);
+    int senderr = sendto(descriptor, cadena ,sizeof(char)*strlen(cadena),0, (struct sockaddr*)&addr, sizeaddr);
+    if(senderr<0){
+        error("sendto()");
+    }
+    int recverr = recvfrom(descriptor, mensaje, sizeof(char)*512,0, (struct sockaddr*)&addr, &sizeaddr);
+    if(recverr < 0){
+        error("recvfrom()");
+    }
     printf("%s",mensaje);
-    close(descriptor);
+    int closeerr = close(descriptor);
+    if(closeerr < 0){
+        error("close()");
+    }
     return 0;
 }
