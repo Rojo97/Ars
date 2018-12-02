ce8978f3e380228a3286ff9081958f5aa76645d2
Rojo97 <victorrojo1997@gmail.com>
2018-12-02 13:32:44 +0100

    Control de paquete error

--- Practica3/ClienteTFTP.c
+++ Practica3/ClienteTFTP.c
@@ -11,8 +11,6 @@
 #include <unistd.h>
 
 /** TODO
- * - enviar archivos
- * - Comprobación de errores (paquete error)
  * - Comentarios
  * - Borrar el archivo en caso de error ya que es corrupto
  * - 100 caractes nombre
@@ -20,8 +18,7 @@
 */
 
 #define BUFFSIZE 516 //Tamaño maximo a recibir o enviar
-#define ACKSIZE 4
-#define MODO "octet"
+#define ACKSIZE 4 //Tamaño del paquete ack
 
 /*  Error llamada tras comprobar que alguna fución ha devuelto un error
     Recibe como parametro una cadena que indica el nombre de la función que ha fallado
@@ -35,37 +32,43 @@ void error(char *funcion){
 int main(int argc, char **argv)
 {
     /*Declaración de las variables a utilizar*/
-    short vFlag = 0;
-    int tamBlock = BUFFSIZE;
-    unsigned char rw;
-    short port = 0; //Guarda el puerto de destino en network byte order
+
+    int tamBlock = BUFFSIZE;    //Tamaño que se está enviado o recibiendo
     int descriptor = 0; //Descriptor del socket
+    unsigned char rw; //Lectura o escritura
+    char mode[] = "octet"; //Modo de transmisión
     char sendBuff[BUFFSIZE]; //Cadena del mensaje recibido
     char recvBuff[BUFFSIZE]; //Mensaje enviado
-    char ack[ACKSIZE]; 
+    char ack[ACKSIZE]; //Ack para confirmar la recepción de mensajes
     struct sockaddr_in localIp; //Dirección local
     struct sockaddr_in remoteIp;    //Dirección destino
     struct in_addr ip;  //Ip de destino en network byte order
-    short expected = 1;
-    short recived;
+    short port = 0; //Guarda el puerto de destino en network byte order
+    short expected = 1; //Numero de paquete experado
+    short recived; //Numero de paquete recibido
+    short type; //tipo de paquete recibido
+    short vFlag = 0;    //Controla si la opcion -v está activada
     socklen_t sizeRemoteIp; //Tamaño del struct sockaddr_in remoteIp
-    FILE *file;
+    FILE *file;  //Archivo de lectura o escritura
 
     /*Comprobación de argumentos*/
     if(argc > 3|| argc < 6){
-        if(strcmp(argv[2], "-r") == 0){
+        if(strcmp(argv[2], "-r") == 0){  //Lectura
             rw = 1;
-        } else if(strcmp(argv[2], "-w") == 0){
+        } else if(strcmp(argv[2], "-w") == 0){ //Escritura
             rw = 2;
         } else {
-            printf("Uso incorrecto, el uso del programa es el siguiente:\nqotd-udp-client-Rojo-Alvarez direccion.IP.servidor [-p puerto-servidor]\n");
+            printf("Uso incorrecto, el uso del programa es el siguiente:\ntftp-client ip-servidor {-r|-w} archivo [-v]\n");
+            return(-1);
+        } 
+        if(argc == 5 && strcmp(argv[4], "-v")==0){ //Modo -v
+            vFlag = 1;
+        } else {
+            printf("Uso incorrecto, el uso del programa es el siguiente:\ntftp-client ip-servidor {-r|-w} archivo [-v]\n");
             return(-1);
         }
-        if(argc == 5 && strcmp(argv[4], "-v")==0){
-            vFlag = 1;
-        }
     } else { //Argumentos incorrectos
-        printf("Numero de argumentos incorrecto, el uso del programa es el siguiente:\nqotd-udp-client-Rojo-Alvarez direccion.IP.servidor [-p puerto-servidor]\n");
+        printf("Numero de argumentos incorrecto, el uso del programa es el siguiente:\ntftp-client ip-servidor {-r|-w} archivo [-v]\n");
         return(-1);
     }
 
@@ -107,7 +110,7 @@ int main(int argc, char **argv)
     memset(recvBuff,0, BUFFSIZE);
     memcpy(sendBuff+1, &rw, 1);
     strcpy(sendBuff+2, argv[3]);
-    strcpy(sendBuff+3+strlen(argv[3]), MODO);
+    strcpy(sendBuff+3+strlen(argv[3]), mode);
     ack[0] = '\0';
     ack[1] = '\4';
     /*Enviamos un mensaje con el texto contenido en cadena al destino indicado*/
@@ -118,7 +121,7 @@ int main(int argc, char **argv)
             printf("Enviada solicitud de escritura de %s a servidor tftp en %s.\n", argv[1], argv[3]);
         }
     }
-    int senderr = sendto(descriptor, &sendBuff ,(strlen(argv[3])+ 4 +strlen(MODO)),0, (struct sockaddr*)&remoteIp, sizeRemoteIp);
+    int senderr = sendto(descriptor, &sendBuff ,(strlen(argv[3])+ 4 +strlen(mode)),0, (struct sockaddr*)&remoteIp, sizeRemoteIp);
     if(senderr<0){
         error("sendto()");
     }
@@ -132,16 +135,17 @@ int main(int argc, char **argv)
             }
             /*Recibimos la respuesta y la guardamos en mensaje*/
             if(vFlag==1) printf("Recibido bloque del servidor tftp.\n"); 
-         tamBlock = recverr;
+            tamBlock = recverr;
+            type = (unsigned char) recvBuff[0]*256 + (unsigned char) recvBuff[1];
             recived =(unsigned char) recvBuff[2]*256 + (unsigned char) recvBuff[3];
-            if(vFlag==1){
+            if(vFlag==1 && type == 3){
                 if(recived == 1){
                     printf("Es el primer bloque (numero de bloque 1).\n");
                 } else if(recived!= 1){
                     printf("Es el bloque con codigo %d.\n", recived);
                 } 
             }
-            if(expected == recived){
+            if(expected == recived && type == 3){
                 ack[2] = recvBuff[2];
                 ack[3] = recvBuff[3];
                 if(vFlag==1) printf("Enviamos el ACK del bloque %d.\n", recived);
@@ -151,9 +155,12 @@ int main(int argc, char **argv)
                 }
                 fwrite(recvBuff+4,1, tamBlock-4, file);
                 expected++;
-            }else{
-                printf("%s\n", recvBuff+4);
-                exit(0);
+            }else if(type == 5){
+                printf("Error: %s\n", recvBuff+4);
+                exit(EXIT_FAILURE);
+            } else{
+                printf("Error desconocido\n");
+                exit(EXIT_FAILURE);
             }
         }
     } else if(strcmp(argv[2], "-w")==0){
@@ -167,17 +174,17 @@ int main(int argc, char **argv)
                 error("recvfrom()");
             }
             /*Recibimos la respuesta y la guardamos en mensaje*/
-            if(vFlag==1) printf("Recibido ACK del servidor tftp.\n"); 
-            //tamBlock = recverr;
+            type = (unsigned char) recvBuff[0]*256 + (unsigned char) recvBuff[1];
+            if(vFlag==1 && type == 4) printf("Recibido ACK del servidor tftp.\n"); 
             recived =(unsigned char) recvBuff[2]*256 + (unsigned char) recvBuff[3];
-            if(vFlag==1){
+            if(vFlag==1 && type == 4){
                 if(recived == 0){
                     printf("Es el primer ACK (numero de ACK %x).\n", recived);
                 } else if(recived!= 0){
                     printf("Es el ACK con codigo %d.\n", recived);
                 } 
             }
-            if(expected == recived){
+            if(expected == recived && type == 4){
                 expected++;
                 sendBuff[2] = expected/256;
                 sendBuff[3] = expected%256;
@@ -187,9 +194,12 @@ int main(int argc, char **argv)
                 if(senderr<0){
                     error("sendto()");
                 }
-            }else {
+            }else if(type == 5) {
                 printf("%s\n", recvBuff+4);
-                exit(0);
+                exit(EXIT_FAILURE);
+            } else {
+                printf("Error desconocido\n");
+                exit(EXIT_FAILURE);
             }
         }
             int recverr = recvfrom(descriptor, &recvBuff, BUFFSIZE, 0, (struct sockaddr*)&remoteIp, &sizeRemoteIp);
